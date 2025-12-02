3a8d4d8587bdec0fe9a34c58bbe7397004b1999d
wescleiab <wescleiab@gmail.com>
2025-12-02 08:32:29 -0300

    Update supertrunfo.c

--- supertrunfo.c
+++ supertrunfo.c
@@ -38,5 +38,32 @@ int main()
         printf("O PIB e: %.2f\n" , pib);
         printf("A quantidade de pontos turisticos e: %d\n" , pontos);
 
+        /*Inserindo as Informções da carta 02*/
+    
+        printf("Insira os Dados da Carta 1\n");
+        printf("Insira o Estado do estado de A ate H:");
+    scanf("%s", &estado);    
+        printf("Insira o codigo da carta\n");
+    scanf("%s" , &carta);
+     printf("Insira o nome da cidade\n");
+    scanf("%s", &cidade);
+        printf("Insira o total da população\n");
+    scanf("%d", &população);
+        printf("Insira a area da cidade em km\n");
+    scanf("%f" , &area);
+     printf("Insira o PIB\n");
+    scanf("%f" , &pib);
+        printf("Insira os pontos turisticos\n");
+    scanf("%d" , &pontos);
+
+    /*Imprimindo as informações da carta 02*/
+        printf("O estado e: %s\n", &estado);
+        printf("O codigo da carta e: %s\n" , &carta);
+        printf("O nome da cidade é : %s\n", &cidade);
+        printf("O total da populacao é: %d\n", população);
+        printf("A area total e: %.2f\n" , area);
+        printf("O PIB e: %.2f\n" , pib);
+        printf("A quantidade de pontos turisticos e: %d\n" , pontos);
+
     return 0;
 }
